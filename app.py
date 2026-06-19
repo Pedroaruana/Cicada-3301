e7c777de9f4119ba6063f0dcf982ac71331fc769
PEDRO <aruanapedro@gmail.com>
2026-06-19 19:48:53 -0300

    feat: pagina de instrucoes com texto e estilo de documento

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app.secret_key = 'cicada-3301-secret'
 def index():
     return render_template('index.html')
 
+@app.route('/instrucoes')
+def instrucoes():
+    return render_template('instrucoes.html')
+
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5000))
